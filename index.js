f34f08849557f188f983f406e5fe26306292c5b2
Ryan Day <soldair@gmail.com>
2013-02-11 02:39:40 -0800

    wip's for client models... sleepy

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-var printSpool = require('./lib/printSpool')
+var printerManager = require('./lib/printerManager')
+, printSpool = require('./lib/printSpool')
 , printasticProtocol = require('printastic-protocol');
 , net = require('net')
 ;
 
 module.exports = function(config){
+  // todo spools entity
+  // queue
+  // calls getPrinter on clients.
+  config.spools.forEach(function(){
 
+  });
+
+  var server = net.createServer(function(client){
+    var serverStream = printasticProtocol();
+    client.pipe(serverStream).pipe(client);
+
+    serverStream.on('message',function(message){
+      message,type == "printer"
+    });
+
+  });
+
+  return server;
 }
